a2e313fde22a6f73d69c3d82894360892ac92d20
mauzus <mauzus@0087f35e-8548-11de-96fc-292cd5189532>
2009-08-11 04:22:11 +0000

    arriving Dollet

--- trunk/Lua/Dialogue Skip.lua
+++ trunk/Lua/Dialogue Skip.lua
@@ -1,40 +1,33 @@
 -- press the X button at the first possible
 -- frame after each box of dialogue
 
-mem1 = 0x83054
-mem2 = 0x83018
-mem3 = 0x83014
-mem1Pressed = 0
-mem2Pressed = 0
-mem3Pressed = 0
+pReady = { 0x83014, 0x83018, 0x83054, 0x830CC, 0x8308C, 0x83090 }
+btPressed = {    0,       0,       0,       0,       0,       0 }
 
 while (true) do
-	if memory.readbyte(mem1) == 1 then
-		if mem1Pressed == 0 then
-			joypad.set(1,{["x"]=true})
-			mem1Pressed = 1
+
+	for i = 1,# btPressed do
+		ready = memory.readbyte(pReady[i])
+		if ready == 1 then
+			if btPressed[i] == 0 or btPressed[i] == 10 then
+				joypad.set(1,{["x"]=true})
+				btPressed[i] = 1
+			end
+		elseif ready == 10 then
+			if btPressed[i] == 0 or btPressed[i] == 1 then
+				joypad.set(1,{["x"]=true})
+				btPressed[i] = 10
+			end
+		else
+			btPressed[i] = 0
 		end
-	else
-		mem1Pressed = 0
+--		gui.text(40,10+(i*8),btPressed[i] .. " " .. ready)
 	end
 
-	if memory.readbyte(mem2) == 1 then
-		if mem2Pressed == 0 then
-			joypad.set(1,{["x"]=true})
-			mem2Pressed = 1
-		end
-	else
-		mem2Pressed = 0
-	end
-
-	if memory.readbyte(mem3) == 10 then
-		if mem3Pressed == 0 then
-			joypad.set(1,{["x"]=true})
-			mem3Pressed = 1
-		end
-	else
-		mem3Pressed = 0
-	end
+--	mem  = 0x83088
+--	for i=0,32 do
+--		gui.text(40,10+(i*8),mem+(i*4) .. " " .. memory.readbyte(mem+(i*4)))
+--	end
 
 	emu.frameadvance()
 end
