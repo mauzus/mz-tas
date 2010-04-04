ccb358ac67aa5411f7113b6eb676c3dee7d48ec6
mauzus <mauzus@0087f35e-8548-11de-96fc-292cd5189532>
2010-04-04 16:43:21 +0000

    add experimental PCSX-RR build

---
+++ PCSX/LuaEngine.h
@@ -0,0 +1,36 @@
+#ifndef _LUAENGINE_H
+#define _LUAENGINE_H
+
+enum LuaCallID
+{
+	LUACALL_BEFOREEMULATION,
+	LUACALL_AFTEREMULATION,
+	LUACALL_BEFOREEXIT,
+
+	LUACALL_COUNT
+};
+void CallRegisteredLuaFunctions(int calltype);
+
+//void PCSX_LuaWrite(uint32 addr);
+void PCSX_LuaFrameBoundary();
+int PCSX_LoadLuaCode(const char *filename);
+void PCSX_ReloadLuaCode();
+void PCSX_LuaStop();
+int PCSX_LuaRunning();
+
+int PCSX_LuaUsingJoypad(int);
+uint32 PCSX_LuaReadJoypad(int);
+int PCSX_LuaSpeed();
+//int PCSX_LuaFrameskip();
+int PCSX_LuaRerecordCountSkip();
+
+void PCSX_LuaGui(void *s, int width, int height, int bpp, int pitch);
+
+void PCSX_LuaWriteInform();
+
+void PCSX_LuaClearGui();
+void PCSX_LuaEnableGui(uint8 enabled);
+
+char* PCSX_GetLuaScriptName();
+
+#endif
