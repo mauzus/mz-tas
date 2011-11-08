9158f1fa0b5bd4037f8d23dad3ee36e7ef93bd9d
mauzus <mauzus@0087f35e-8548-11de-96fc-292cd5189532>
2011-11-08 04:05:37 +0000

    add Tails Adventures stuff for maps

---
+++ trunk/Tails/map_info.php
@@ -0,0 +1,11 @@
+<?php
+$map_info = array (
+"1-1" => array("level_height" => 4,"level_width" => 15,"height_crop" => 17,"width_extra" => $osd_width),
+"1-2" => array("level_height" => 4,"level_width" => 15,"height_crop" => 17,"width_extra" => $osd_width),
+"1-3" => array("level_height" => 4,"level_width" =>  7,"height_crop" => 17,"width_extra" => $osd_width+15),
+"1-4" => array("level_height" => 6,"level_width" =>  9,"height_crop" => 17,"width_extra" => $osd_width),
+"2-1" => array("level_height" => 4,"level_width" => 15,"height_crop" => 17,"width_extra" => $osd_width),
+"2-2" => array("level_height" => 4,"level_width" => 15,"height_crop" => 17,"width_extra" => $osd_width),
+"3-1" => array("level_height" => 19,"level_width" => 3,"height_crop" => 17+24,"width_extra" => $osd_width),
+"3-2" => array("level_height" => 4,"level_width" =>  5,"height_crop" => 17,"width_extra" => $osd_width),
+);
