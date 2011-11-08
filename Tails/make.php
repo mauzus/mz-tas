1cbd1c39459b1f60197cd14f35a65237a2924da3
mauzus <mauzus@0087f35e-8548-11de-96fc-292cd5189532>
2011-11-08 04:05:37 +0000

    add Tails Adventures stuff for maps

---
+++ Tails/make.php
@@ -0,0 +1,138 @@
+<?php
+$map              = "1-4";
+//$debug_lines      = 1;
+$print_in_browser = 1;
+
+$osd_width    = 32; $osd_height   = 40;
+require('map_info.php');
+if (!isset($map_info[$map]))
+	exit("<b>ERROR:</b> There's no info for stage <b>".$map."</b>.");
+$level_height = $map_info[$map]['level_height'];
+$level_width  = $map_info[$map]['level_width'];
+$height_crop  = $map_info[$map]['height_crop'];
+$width_extra  = $map_info[$map]['width_extra'];
+
+//$level_height = 4;
+//$level_width  = 15;
+//
+//$height_crop  = 0;
+//$height_crop  = 17; // horizontal levels
+////$height_crop  = 41; // vertical levels
+//$width_extra  = 160 - $osd_width;
+//$width_extra  = $osd_width;
+
+$screen_width = 160 - $osd_width;
+$screen_height = 144 - $osd_height;
+$shots_dir = "shots/" . $map . "/";
+$shots_dir_new = "temp/";
+
+for ($j = 0; $j <= $level_height; $j++) {
+	for ($i = 0; $i <= $level_width; $i++) {
+		$src_x = $osd_width;
+		$src_y = $osd_height;
+		$img_width  = $screen_width;
+		$img_height = $screen_height;
+		if ($i == 0) { $src_x = 0; $img_width  += $osd_width; }
+		if ($j == 0) { $src_y = 0; $img_height += $osd_height; }
+		cut_image($j, $i, $src_x, $src_y, $img_width, $img_height);
+	}
+}
+
+create_map($map, $level_width+1,$level_height+1);
+
+foreach (glob($shots_dir_new."*.png") as $filename) {
+	unlink($filename);
+}
+
+function cut_image($y, $x, $src_x, $src_y, $screen_width, $screen_height) {
+	global $shots_dir, $shots_dir_new;
+
+	$dega_shots_dir = "E:/MinGW/projects/dega/shots/";
+	$filename = str_pad($y, 3, "0", STR_PAD_LEFT) . "_" . str_pad($x, 3, "0", STR_PAD_LEFT) . ".png";
+	$dega_filename = $dega_shots_dir."Tails Adventures (JU) [!]_".str_pad($x, 3, "0", STR_PAD_LEFT).".png";
+
+	if (!file_exists($shots_dir))
+		mkdir($shots_dir);
+	if (file_exists($dega_filename) && !file_exists($shots_dir.$filename)) {
+//		if (file_exists($shots_dir.$filename))
+//			unlink($shots_dir.$filename);
+		rename($dega_filename, $shots_dir.$filename);
+	}
+	if (!file_exists($shots_dir.$filename)) {
+//		echo '<h2>"'.$shots_dir.$filename.'" doesn\'t exist! Image was not created.</h2>';
+//		return;
+		$img = imagecreatetruecolor(160, 144);
+	}
+	else {
+		$img     = imagecreatefrompng($shots_dir.$filename);
+		if ($img === false) {
+			echo '<h2>"'.$shots_dir.$filename.'" is damaged! Image was not created.</h2>';
+			return;
+		}
+	}
+
+	// create a new temporary image
+	$tmp_img = imagecreatetruecolor($screen_width, $screen_height);
+
+	// copy and resize old image into new image 
+	imagecopy($tmp_img, $img, 0, 0, $src_x, $src_y, $screen_width, $screen_height);
+
+	// save image into a file
+	imagepng($tmp_img, $shots_dir_new.$filename);
+
+	// print image directly
+//header('Content-type: image/png');
+//imagepng($tmp_img, null);
+}
+
+function create_map($map_filename, $size_x, $size_y) {
+	global $osd_width, $osd_height, $screen_width, $screen_height, $shots_dir_new, $height_crop, $width_extra;
+	global $debug_lines, $print_in_browser;
+
+	// create a new temporary image
+	$img_width = ($size_x-1)*$screen_width+$width_extra+$osd_width;
+	$img_height = $size_y*$screen_height-$osd_height-$height_crop;
+	$tmp_img = imagecreatetruecolor($img_width, $img_height);
+	$line_color = imagecolorallocate($tmp_img, 255, 0, 0);
+
+	$dest_y = 0;
+	for ($j = 0; $j < $size_y; $j++) {
+		$dest_x = 0;
+		for ($i = 0; $i < $size_x; $i++) {
+			$filename = str_pad($j, 3, "0", STR_PAD_LEFT) . "_" . str_pad($i, 3, "0", STR_PAD_LEFT) . ".png";
+			if (!file_exists($shots_dir_new.$filename)) {
+				echo '<h2>"'.$shots_dir_new.$filename.'" doesn\'t exist!! Image was not created.</h2>';
+				return;
+			}
+			$img     = imagecreatefrompng($shots_dir_new.$filename);
+			if ($img === false) {
+				echo '<h2>"'.$shots_dir_new.$filename.'" is damaged!! Image was not created.</h2>';
+				return;
+			}
+			// copy and resize old image into new image
+			$src_width  = $screen_width;
+			$src_height = $screen_height;
+			if ($i == 0) $src_width  += $osd_width;
+			if ($j == 0) $src_height += $osd_height;
+			if ($i == ($size_x-1)) $dest_x = $img_width-$screen_width;
+			imagecopy($tmp_img, $img, $dest_x, $dest_y, 0, 0, $src_width, $src_height);
+			if (isset($debug_lines) && $debug_lines) {
+				imageline($tmp_img, $dest_x, 0, $dest_x, $img_height, $line_color);
+				imagestring($tmp_img, 1, $dest_x+5, $dest_y+5, $j.":".$i, $line_color);
+			}
+			$dest_x += $src_width;
+		}
+		if (isset($debug_lines) && $debug_lines) {
+			imageline($tmp_img, 0, $dest_y, $img_width, $dest_y, $line_color);
+		}
+		$dest_y += $src_height;
+	}
+	// save image into a file
+	imagepng($tmp_img, $map_filename.".png");
+
+	// print image directly
+	if (isset($print_in_browser) && $print_in_browser) {
+		if (file_exists($map_filename.".png"))
+			echo '<img src="'.$map_filename.'.png" />';
+	}
+}
---
+++ Tails/mapper.lua
@@ -0,0 +1,281 @@
+--$D27C -> $D27D	-	Minimum camera Y position
+--$D27E -> $D27F  -	Maximum camera Y position
+--$D280 -> $D281	-	Minimum camera X position
+--$D282 -> $D283	-	Maximum camera X position
+--$D2AD			-	Current level
+--$D2AE			-	Current Act
+local level = 1
+local act = 1
+
+local height_multiplier = 0
+
+local last_screenshot_x = 8978978979879879
+local screenshot_width = 160 - 32
+local screenshot_heigth = 144 - 40
+local fixed_cam_y = (screenshot_heigth*height_multiplier)+8
+local fixed_cam_x = 0
+
+local cam_lock = 0
+
+local address = {
+	cam_x           = 0xD288,
+	cam_y           = 0xD28A,
+	invulnerability = 0xD3A1,
+	flight_gauge    = 0xD3AF,
+
+	tails           = 0xD500,
+	objects         = 0xD500
+}
+local offset = {
+	flags1    = 0x03,
+	flags2    = 0x04,
+	x         = 0x11,
+	y         = 0x14,
+	spd_sub_x = 0x16,
+	spd_x     = 0x17,
+	spd_sub_y = 0x18,
+	spd_y     = 0x19,
+	scr_x     = 0x1A,
+	scr_y     = 0x1C,
+	hp        = 0x26,
+	size_x    = 0x2c,
+	size_y    = 0x2d
+}
+local value = {
+	objects_num     = 24,
+	tails_hb_size_x = 20,
+	tails_hb_size_y = 13,
+	tails_hb_color  = "#FFFFFF",
+	inv_color       = "#FFFF00",
+	bomb_hb_size    = 16,
+	bomb_hb_color   = "#FF0000",
+	enemy_hb_size   = 16,
+	enemy_hb_color  = "#FFFFFF",
+	hb_transparency = 25,
+	cam_off_x       = 48,
+	cam_off_y       = 24,
+	slot_size       = 0x40
+}
+
+local fixed_x = memory.readword(address.tails+offset.x)
+local fixed_y = memory.readword(address.tails+offset.y)
+
+function GetGlobalValues()
+	-- Camera Position
+	value.cam_x             = memory.readword(address.cam_x)
+	value.cam_y             = memory.readword(address.cam_y)
+end
+
+function GetObjectValues(obj_value,slot_n)
+		obj_value.slot_offset = slot_n * value.slot_size
+		obj_value.obj_id      = memory.readbyte(address.objects+obj_value.slot_offset)
+		obj_value.flags2      = memory.readbyte(address.objects+offset.flags2+obj_value.slot_offset)
+		obj_value.hp          = string.format("%x",(memory.readbytesigned(address.objects+offset.hp+obj_value.slot_offset)))
+		obj_value.x           = memory.readword(address.objects+offset.x+obj_value.slot_offset)
+		obj_value.y           = memory.readword(address.objects+offset.y+obj_value.slot_offset)
+		obj_value.hb_size_x   = memory.readbyte(address.objects+offset.size_x+obj_value.slot_offset)
+		obj_value.hb_size_y   = memory.readbyte(address.objects+offset.size_y+obj_value.slot_offset)
+		obj_value.rel_x       = obj_value.x-value.cam_x - value.cam_off_x
+		obj_value.rel_y       = obj_value.y-value.cam_y - value.cam_off_y
+		obj_value.hb_rel_x    = obj_value.rel_x
+		obj_value.hb_rel_y    = obj_value.rel_y
+		obj_value.spd_x       = memory.readwordsigned(address.objects+offset.spd_x+obj_value.slot_offset)
+		obj_value.spd_y       = memory.readwordsigned(address.objects+offset.spd_y+obj_value.slot_offset)
+end
+
+
+function ObjectInformation()
+	local	count = 0
+	for slot_n = 0,value.objects_num-1 do
+		local obj_value = {}
+		local color_to_use
+		GetObjectValues(obj_value,slot_n)
+
+		if true then
+			color_to_use = value.enemy_hb_color
+
+			-- object hitbox
+			gui.box(obj_value.hb_rel_x-obj_value.hb_size_x-1,
+			        obj_value.hb_rel_y,
+			        obj_value.hb_rel_x+obj_value.hb_size_x,
+			        obj_value.hb_rel_y-obj_value.hb_size_y,
+			        color_to_use .. value.hb_transparency)
+
+			-- object hp
+			gui.text(obj_value.hb_rel_x,obj_value.hb_rel_y,obj_value.obj_id..":"..obj_value.hp)
+
+			-- position axis
+			local AXIS_COLOUR           = 0xFFFFFF
+			local AXIS_SIZE             = 2
+			gui.drawline(obj_value.hb_rel_x, obj_value.hb_rel_y-AXIS_SIZE, obj_value.hb_rel_x, obj_value.hb_rel_y+AXIS_SIZE, AXIS_COLOUR)
+			gui.drawline(obj_value.hb_rel_x-AXIS_SIZE, obj_value.hb_rel_y, obj_value.hb_rel_x+AXIS_SIZE, obj_value.hb_rel_y, AXIS_COLOUR)
+
+		end -- if end
+
+	end -- for end
+end
+
+gui.register(function()
+	GetGlobalValues()
+	ObjectInformation()
+
+	local x_coord = 0
+	for i = 0,16 do
+		x_coord = (screenshot_width*i) - value.cam_x + 32
+		if i==0 then x_coord = 0 end
+		gui.line(x_coord,
+		         0,
+		         x_coord,
+		         144,
+		         "red")
+	end
+
+	local displ = 5
+	for i = 0,20 do
+		gui.line(0,
+		         (40+32+screenshot_heigth*i) - value.cam_y - value.cam_off_y,
+		         160,
+		         (40+32+screenshot_heigth*i) - value.cam_y - value.cam_off_y,
+		        "red")
+		for j = 0,16 do
+			x_coord = (48+screenshot_width*j) - value.cam_x - value.cam_off_x + displ + 32
+			if j==0 then x_coord = x_coord - 32 end
+			gui.text(
+			x_coord,
+			(40+32+screenshot_heigth*i) - value.cam_y - value.cam_off_y + displ,
+			i..":"..j)
+		end
+	end
+
+	local gui_x, gui_y = 40,2
+	local cam_lock_str = ""
+	if cam_lock ~= 0 then cam_lock_str = "*" end
+	gui.text(gui_x, gui_y+ 0, "lvl: "..  level.."-"..act)
+	gui.text(gui_x, gui_y+ 8, "pos: "..  fixed_x..":"..fixed_y)
+	gui.text(gui_x, gui_y+16, "cam: "..  value.cam_x..":"..value.cam_y-8)
+	gui.text(gui_x, gui_y+24, "hgt: "..  height_multiplier .." "..cam_lock_str)
+end)
+
+local level_pressed    = 0
+local act_pressed      = 0
+local cam_lock_pressed = 0
+
+emu.registerafter( function()
+	GetGlobalValues()
+
+	-- manual tails movement
+	local keys = input.get()
+	local manual_spd = 1
+	if keys.J then
+		fixed_x = fixed_x-manual_spd
+	end
+	if keys.L then
+		fixed_x = fixed_x+manual_spd
+	end
+	if keys.I then
+		fixed_y = fixed_y-manual_spd
+	end
+	if keys.K then
+		fixed_y = fixed_y+manual_spd
+	end
+
+	if (((value.cam_x%screenshot_width) == 0) and (last_screenshot_x ~= value.cam_x) and (cam_lock ~= 0)) then
+		last_screenshot_x = value.cam_x
+		gui.savescreenshot()
+	end
+
+	if cam_lock == 0 then
+		height_multiplier = math.floor(math.abs((fixed_y-72) / 104))
+	end
+--	if keys.X and height_pressed == 0 then
+--		height_multiplier = height_multiplier + 1
+--		fixed_cam_y = (screenshot_heigth*height_multiplier)+8
+--		height_pressed = 1
+--	end
+--	if keys.Z and height_pressed == 0 then
+--		height_multiplier = height_multiplier - 1
+--		fixed_cam_y = (screenshot_heigth*height_multiplier)+8
+--		height_pressed = 1
+--	end
+--	if ((not keys.X) and (not keys.Z)) then
+--		height_pressed = 0
+--	end
+
+	if keys.H and level_pressed == 0 then
+		level = level + 1
+		level_pressed = 1
+	end
+	if keys.F and level_pressed == 0 then
+		level = level - 1
+		level_pressed = 1
+	end
+	if ((not keys.H) and (not keys.F)) then level_pressed = 0 end
+	if keys.T and act_pressed == 0 then
+		act = act + 1
+		act_pressed = 1
+	end
+	if keys.G and act_pressed == 0 then
+		act = act - 1
+		act_pressed = 1
+	end
+	if ((not keys.T) and (not keys.G)) then act_pressed = 0 end
+
+	if keys.C and cam_lock_pressed == 0 then
+		if cam_lock ~= 0 then cam_lock = 0
+		else
+			cam_lock = 1
+			height_multiplier = math.floor(math.abs((fixed_y-72) / 104))
+			fixed_cam_y = (screenshot_heigth*height_multiplier)+8
+		end
+		cam_lock_pressed = 1
+	end
+	if (not keys.C) then
+		cam_lock_pressed = 0
+	end
+
+	if keys.M then
+		os.execute('del /Q shots')
+	end
+
+	-- cheats
+	memory.writebyte(0xd3a1, 0xff)
+	memory.writeword(0xd511, fixed_x)
+	memory.writeword(0xd514, fixed_y)
+	if cam_lock ~= 0 then memory.writeword(address.cam_y, fixed_cam_y) end
+	memory.writebyte(0xd506, 0)
+end)
+
+--memory.registerwrite(address.cam_x, 2, function()
+--	memory.writeword(address.cam_x, fixed_cam_x)
+--end)
+
+memory.registerwrite(address.cam_y, 2, function()
+	if cam_lock ~= 0 then memory.writeword(address.cam_y, fixed_cam_y) end
+end)
+
+memory.registerwrite(address.tails+offset.x, 2, function()
+	memory.writeword(address.tails+offset.x, fixed_x)
+end)
+
+memory.registerwrite(address.tails+offset.y, 2, function()
+	memory.writeword(address.tails+offset.y, fixed_y)
+end)
+
+-- animation
+memory.registerwrite(0xD506, 1, function()
+	memory.writebyte(0xD506, 0)
+end)
+
+memory.registerwrite(0xD2AD, 1, function()
+	memory.writebyte(0xD2AD, level)
+end)
+
+memory.registerwrite(0xD2AE, 1, function()
+	memory.writebyte(0xD2AE, act)
+end)
+
+while true do
+	memory.writebyte(0xD2AD, level)
+	memory.writebyte(0xD2AE, act)
+	emu.frameadvance()
+end
